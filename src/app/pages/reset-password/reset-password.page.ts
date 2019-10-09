2441de005c9f3a09f4705b240c2f7cfd9a9df79b
miguelleonmarti <miguelleonmarti@gmail.com>
2019-10-09 18:36:57 +0100

    authentication done

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/user/auth.service';
+import { AlertController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPage implements OnInit {
 
-  constructor() { }
+  public resetPasswordForm: FormGroup;
+  constructor(
+    private authService: AuthService,
+    private alertCtrl: AlertController,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.resetPasswordForm = this.formBuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.required, Validators.email]),
+      ],
+    });
+  }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  resetPassword(resetPasswordForm: FormGroup): void {
+    if (!resetPasswordForm.valid) {
+      console.log(
+        'Form is not valid yet, current value:', resetPasswordForm.value
+      );
+    } else {
+      const email: string = resetPasswordForm.value.email;
+      this.authService.resetPassword(email).then(
+        async () => {
+          const alert = await this.alertCtrl.create({
+            message: 'Check your email for a password reset link',
+            buttons: [
+              {
+                text: 'Ok',
+                role: 'cancel',
+                handler: () => {
+                  this.router.navigateByUrl('login');
+                },
+              },
+            ],
+          });
+          await alert.present();
+        },
+        async error => {
+          const errorAlert = await this.alertCtrl.create({
+            message: error.message,
+            buttons: [{ text: 'Ok', role: 'cancel' }],
+          });
+          await errorAlert.present();
+        }
+      );
+    }
   }
 
 }
