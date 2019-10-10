ef533a72d0f70190a867a5d1746e28e9351df894
miguelleonmarti <miguelleonmarti@gmail.com>
2019-10-10 15:38:21 +0100

    pipes deketed

--- src/app/pages/event-list.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EventListPipe } from './event-list.pipe';
-
-describe('EventListPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EventListPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
