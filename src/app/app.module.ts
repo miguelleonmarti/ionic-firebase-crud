3b0bae4e5d4804cd537bd2cc722899d1c2bebd07
miguelleonmarti <miguelleonmarti@gmail.com>
2019-10-15 08:48:54 +0100

    creating new events functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { EventCreatePipe } from './pages/event-create.pipe';
-import { EventDetailPipe } from './pages/event-detail.pipe';
-import { EventListPipe } from './pages/event-list.pipe';
-import { LoginPipe } from './pages/login.pipe';
-import { ProfilePipe } from './pages/profile.pipe';
-import { ResetPasswordPipe } from './pages/reset-password.pipe';
-import { SignupPipe } from './pages/signup.pipe';
 
 @NgModule({
-  declarations: [AppComponent, EventCreatePipe, EventDetailPipe, EventListPipe, LoginPipe, ProfilePipe, ResetPasswordPipe, SignupPipe],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
--- src/app/pages/event-create/event-create.page.ts
+++ src/app/pages/event-create/event-create.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+// services
+import { EventService } from '../../services/event/event.service';
+
+// router
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-event-create',
   templateUrl: './event-create.page.html',
-  styleUrls: ['./event-create.page.scss'],
+  styleUrls: ['./event-create.page.scss']
 })
 export class EventCreatePage implements OnInit {
+  constructor(private router: Router, private eventService: EventService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  createEvent(
+    eventName: string,
+    eventDate: string,
+    eventPrice: number,
+    eventCost: number
+  ): void {
+    if (
+      eventName === undefined ||
+      eventDate === undefined ||
+      eventPrice === undefined ||
+      eventCost === undefined
+    ) {
+      return;
+    }
+
+    this.eventService
+      .createEvent(eventName, eventDate, eventPrice, eventCost)
+      .then(() => this.router.navigateByUrl(''));
   }
-
 }
